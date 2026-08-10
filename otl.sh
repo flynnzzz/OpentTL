c46249407a770b9b6321d10d4a24b08674ea20b6
flynux <flynnzzz@users.noreply.github.com>
2026-08-10 02:17:46 +0200

    bash: updated run script to resolve warnings

--- otl.sh
+++ otl.sh
@@ -1,7 +1,7 @@
 MODULE_PATH="./lib/linux"
 ADD_MODULES="javafx.controls"
 JAR_FILE="./target/opentierlist.jar"
-LOG_FILE="./otl.log"
+LOG_FILE="./log/otl.log"
 
 if [ ! -d "$MODULE_PATH" ]; then
   echo "[ERROR] --- /lib folder does not exist ---"
@@ -11,7 +11,7 @@ if [ ! -f "$JAR_FILE" ]; then
   echo "[ERROR] --- Jar file not found at: $JAR_FILE ---"
 fi
 
-nohup java --module-path $MODULE_PATH \
+nohup java --enable-native-access=javafx.graphics --module-path $MODULE_PATH \
   --add-modules $ADD_MODULES \
   -jar $JAR_FILE \
   >>$LOG_FILE 2>&1 &
