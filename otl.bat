2817a26d7f1d42097eb7ebd1a7bd6178a17c3b34
flynux <flynnzzz@users.noreply.github.com>
2026-08-10 12:47:50 +0200

    batch: updated to match bash script

--- otl.bat
+++ otl.bat
@@ -4,8 +4,8 @@ setlocal
 set "MODULE_PATH=.\lib\windows"
 set "BINARIES=.\lib\windows\dll"
 set "ADD_MODULES=javafx.controls"
-set "JAR_FILE=.\target\opentierlist.jar"
-set "LOG_FILE=.\otl.log"
+set "JAR_FILE=./target\opentierlist.jar"
+set "LOG_FILE=.log\otl.log"
 
 if not exist "%MODULE_PATH%\" (
     echo [ERROR] --- lib folder does not exist ---
@@ -15,6 +15,6 @@ if not exist "%JAR_FILE%" (
     echo [ERROR] --- Jar file not found at: %JAR_FILE% ---
 )
 
-start "" /B java "-Djava.library.path=%BINARIES%" --module-path "%MODULE_PATH%" --add-modules %ADD_MODULES% -jar "%JAR_FILE%" >> "%LOG_FILE%" 2>&1
+start "" /B java "--enable-native-access=javafx.graphics" "-Djava.library.path=%BINARIES%" --module-path "%MODULE_PATH%" --add-modules %ADD_MODULES% -jar "%JAR_FILE%" >> "%LOG_FILE%" 2>&1
 
 endlocal
