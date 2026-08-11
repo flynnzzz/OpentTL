4c433002432e8dd68d2a0b148333013b0d6c26d0
flynnz <196103144+flynnzzz@users.noreply.github.com>
2026-08-11 12:04:40 +0200

    batch: fixed batch file formatting bug

--- otl.bat
+++ otl.bat
@@ -1,20 +1,28 @@
 @echo off
 setlocal
 
-set "MODULE_PATH=.\lib\windows"
-set "BINARIES=.\lib\windows\dll"
+set "MODULE_PATH=.\lib\windows\"
+set "BINARIES=.\lib\windows\dll\"
 set "ADD_MODULES=javafx.controls"
 set "JAR_FILE=.\target\opentierlist.jar"
-set "LOG_FILE=.log\otl.log"
+set "LOG_FILE=.\log\otl.log"
 
 if not exist "%MODULE_PATH%\" (
     echo [ERROR] --- lib folder does not exist ---
+    exit /b 1
 )
 
 if not exist "%JAR_FILE%" (
     echo [ERROR] --- Jar file not found at: %JAR_FILE% ---
+    exit /b 1
 )
 
-start "" /B java "--enable-native-access=javafx.graphics" "-Djava.library.path=%BINARIES%" --module-path "%MODULE_PATH%" --add-modules %ADD_MODULES% -jar "%JAR_FILE%" >> "%LOG_FILE%" 2>&1
+start "" /B java ^
+	"-Djava.library.path="%BINARIES% ^
+	--module-path %MODULE_PATH% ^
+	--add-modules %ADD_MODULES% ^
+	-jar %JAR_FILE% ^
+	>> %LOG_FILE% 2>&1
 
 endlocal
+
