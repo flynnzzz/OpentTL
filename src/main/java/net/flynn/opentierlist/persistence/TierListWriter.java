e4c6559942346f727cd31cd595361eab4cb6a7c0
flynnz <196103144+flynnzzz@users.noreply.github.com>
2026-08-11 18:06:56 +0200

    bugfix: exported png will now have consistent width regardless of window size

--- src/main/java/net/flynn/opentierlist/persistence/TierListWriter.java
+++ src/main/java/net/flynn/opentierlist/persistence/TierListWriter.java
@@ -10,11 +10,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Bounds;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.transform.Scale;
 import net.flynn.opentierlist.model.models.TierList;
+import net.flynn.opentierlist.ui.ConfigHolder;
+import net.flynn.opentierlist.ui.manual.TierBox;
 import net.flynn.opentierlist.ui.manual.TieredPane;
 
 import javax.imageio.ImageIO;
@@ -27,17 +32,24 @@ public class TierListWriter {
 
   private static WritableImage screenshot(TieredPane node) {
 
-    final double inboundWidth = node.getContent().getBoundsInLocal().getWidth(),
-        inboundHeight = node.getContent().getBoundsInLocal().getHeight();
+    final Bounds bounds = node.getContent().getBoundsInLocal();
+    final double fixedWidth = ConfigHolder.DEFAULT_TIERED_BAR_WIDTH + 128,
+                 dynamicWidth = bounds.getWidth();
 
-    final WritableImage image = new WritableImage((int) inboundWidth, (int) inboundHeight);
+    final double targetWidth = Math.min(fixedWidth, dynamicWidth),
+                 inboundHeight = bounds.getHeight(),
+                 x = bounds.getMinX() + (dynamicWidth - targetWidth) / 2;
+
+    final WritableImage image = new WritableImage((int) targetWidth, (int) inboundHeight);
 
     final var params = new SnapshotParameters();
     params.setTransform(new Scale(1, 1));
+    params.setViewport(new Rectangle2D(x, 0, targetWidth, inboundHeight));
 
-    if (Application.getUserAgentStylesheet().equals(new NordDark().getUserAgentStylesheet()))
+    final var darkModeColor = "#2e3440";
+    if (ConfigHolder.getCurrentTheme() == ConfigHolder.Theme.DARK)
       // background color appears white in the screenshot otherwise
-      params.setFill(Color.valueOf("#2e3440"));
+      params.setFill(Color.valueOf((darkModeColor)));
 
     node.hideEditButtons();
 
